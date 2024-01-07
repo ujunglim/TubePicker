8fc1580dddcbc4e0ac16741ed0c4694ee666eba7
yujung <ujunglim@naver.com>
2024-01-07 18:32:53 +0900

    [cmd] add pm2 path

--- Server/run.sh
+++ Server/run.sh
@@ -3,14 +3,15 @@
 echo "Starting PM2 processes..."
 APP_NAME="tube_picker"
 SCRIPT_PATH="app.js"
+PM2="/home/ubuntu/.nvm/versions/node/v17.9.1/bin/pm2"
 
 # Check if the process is running
-if pm2 list | grep -q "$APP_NAME"; then
+if "$PM2" list | grep -q "$APP_NAME"; then
   echo "The process $APP_NAME is already running in PM2."
-  pm2 restart "$APP_NAME"
+  "$PM2" restart "$APP_NAME"
 else
   echo "The process $APP_NAME is not currently running in PM2."
-  pm2 start "$SCRIPT_PATH" --name "$APP_NAME" -- pro
+  "$PM2" start "$SCRIPT_PATH" --name "$APP_NAME" -- pro
 fi
 
 echo "All PM2 processes started."
