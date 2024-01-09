0195e95337ed7f05a6eb762ea54591db5ac4a63a
yujung <ujunglim@naver.com>
2024-01-09 23:54:32 +0900

    [try fix] chomd +x

--- Server/deploy.bat
+++ Server/deploy.bat
@@ -6,6 +6,6 @@ set KEY="C:\Users\Yujung\Documents\TubePicker\aws_key_pair.pem"
 set HOST=ubuntu@ec2-43-203-78-29.ap-northeast-2.compute.amazonaws.com
 @REM copy .env
 scp -i %KEY% ./.env %HOST%:TubePicker/Server
-ssh -i %KEY% %HOST% -t "cd TubePicker; git fetch --all; git reset --hard origin/master; cd Server; bash run.sh"
+ssh -i %KEY% %HOST% -t "cd TubePicker; git fetch --all; git reset --hard origin/master; chmod +x /home/ubuntu/TubePicker/Server/run.sh; /home/ubuntu/TubePicker/Server/run.sh"
 
 echo ===== Finished Deployment =====
--- Server/run.sh
+++ Server/run.sh
@@ -2,16 +2,17 @@
 
 echo "Starting PM2 processes..."
 APP_NAME="tube_picker"
-SCRIPT_PATH="/home/ubuntu/TubePicker/Server/app.js"
+SCRIPT_PATH="app.js"
 PM2="/home/ubuntu/.nvm/versions/node/v17.9.1/bin/pm2"
 
+cd /home/ubuntu/TubePicker/Server/
 # Check if the process is running
-if "$PM2" list | grep -q "$APP_NAME"; then
+if pm2 list | grep -q "$APP_NAME"; then
   echo "The process $APP_NAME is already running in PM2."
-  "$PM2" restart "$APP_NAME"
+  pm2 restart "$APP_NAME"
 else
   echo "The process $APP_NAME is not currently running in PM2."
-  "$PM2" start "$SCRIPT_PATH" --name "$APP_NAME" --max-restarts=1 -- pro
+  pm2 start "$SCRIPT_PATH" --name "$APP_NAME" --max-restarts=1 -- pro
 fi
 
 echo "All PM2 processes started."
