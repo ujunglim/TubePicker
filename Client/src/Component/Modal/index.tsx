bbe6fd00d8d9bcf2eda8a9ca69023ae485dd8f09
yujung <ujunglim@naver.com>
2024-02-14 18:55:18 +0900

    [feat] mask를 layout에서 modal로 이동

--- Client/src/Component/Modal/index.tsx
+++ Client/src/Component/Modal/index.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
+import { useCallback, useEffect } from "react";
 import styles from "./index.module.scss";
-import { useSelector } from "react-redux";
-import { appManage } from "../../store/slices/app";
+import { useSelector, useDispatch } from "react-redux";
+import { appManage, setModalPosition } from "../../store/slices/app";
 import { IoMdClose } from "react-icons/io";
 interface Prop {
   title: string;
@@ -11,18 +12,44 @@ interface Prop {
 }
 const Modal: FC<Prop> = ({ title, children, handleClose, handleOk }) => {
   const { modalPosition } = useSelector(appManage);
+  const dispatch = useDispatch();
+
+  const closeMask = useCallback(() => {
+    dispatch(setModalPosition(undefined));
+    document.body.style.position = ""; // 스크롤바 적용
+    window.scrollTo({ top: modalPosition }); // 스크롤바를 기존의 위치로
+    document.body.style.paddingRight = "0px"; // 화면의 흔들림 커버
+    handleClose();
+  }, [dispatch, handleClose, modalPosition]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeMask();
+      }
+    });
+
+    return () => {
+      window.removeEventListener("keydown", () => closeMask());
+    };
+  }, [closeMask]);
 
   return modalPosition === undefined ? null : (
-    <div className={styles.modal}>
-      <div className={styles.header}>
-        <p className={styles.title}>{title}</p>
-        <button className={styles.closeBtn} onClick={handleClose}>
-          <IoMdClose />
-        </button>
-      </div>
-      <div className={styles.children}>{children}</div>
-      <div className={styles.footer}>
-        <button onClick={handleOk}>OK</button>
+    <div className={styles.container}>
+      {modalPosition !== undefined && (
+        <div className={styles.mask} onClick={closeMask}></div>
+      )}
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <p className={styles.title}>{title}</p>
+          <button className={styles.closeBtn} onClick={handleClose}>
+            <IoMdClose />
+          </button>
+        </div>
+        <div className={styles.children}>{children}</div>
+        <div className={styles.footer}>
+          <button onClick={handleOk}>OK</button>
+        </div>
       </div>
     </div>
   );
--- Client/src/layout/index.tsx
+++ Client/src/layout/index.tsx
@@ -1,42 +1,15 @@
-import { useSelector, useDispatch } from "react-redux";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import styles from "./index.module.scss";
-import { FC, useEffect, useCallback } from "react";
-import { appManage, setModalPosition } from "../store/slices/app";
+import { FC } from "react";
 
 interface prop {
   children: any;
 }
 
 const AppLayout: FC<prop> = ({ children }) => {
-  const { modalPosition } = useSelector(appManage);
-  const dispatch = useDispatch();
-
-  const closeMask = useCallback(() => {
-    dispatch(setModalPosition(undefined));
-    document.body.style.position = ""; // 스크롤바 적용
-    window.scrollTo({ top: modalPosition }); // 스크롤바를 기존의 위치로
-    document.body.style.paddingRight = "0px"; // 화면의 흔들림 커버
-  }, [dispatch, modalPosition]);
-
-  useEffect(() => {
-    window.addEventListener("keydown", (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        closeMask();
-      }
-    });
-
-    return () => {
-      window.removeEventListener("keydown", () => closeMask());
-    };
-  }, [closeMask]);
-
   return (
     <div className={styles.layout}>
-      {modalPosition !== undefined && (
-        <div className={styles.mask} onClick={closeMask}></div>
-      )}
       <Header />
       <div className={styles.content}>
         <SideBar />
