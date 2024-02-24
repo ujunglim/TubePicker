205e1424d3245b384b6e1d111c2491de3dc0b9ac
yujung <ujunglim@naver.com>
2024-02-24 22:42:48 +0900

    [chore] 클라 axios 응답 interceptor수정

--- Client/src/api/index.ts
+++ Client/src/api/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'; 
+import axios, { AxiosError, AxiosResponse } from 'axios'; 
 import getCookies from '../utils/getCookies';
 
 const getBaseUrl = () => {
@@ -17,8 +17,8 @@ const getBaseUrl = () => {
 
 const api: any = axios.create(
   {
-    baseURL: getBaseUrl()
-    // baseURL: 'http://localhost:9090' // 로컬에서 dev할때
+    // baseURL: getBaseUrl()
+    baseURL: 'http://localhost:9090' // 로컬에서 dev할때
   }
 )
 // AxiosRequestConfig
@@ -28,5 +28,30 @@ api.interceptors.request.use((config: any) => {
   return config;
 })
 
+// 응답완료 interceptor
+const onResponse = (
+  response: AxiosResponse,
+): AxiosResponse | Promise<AxiosResponse> => {
+  // access token 갱신 시 토큰 갱신
+  if (response.status === 201) {
+    localStorage.setItem('accessToken', response.data.accessToken);
+  }
+  return response;
+};
 
+// 응답에러 interceptor
+const onResponseError = (err: AxiosError | Error): Promise<AxiosError> => {
+  const error = err as unknown as AxiosError;
+  const { response } = error;
+  const data = response?.data as { msg: string }; // 타입을 명시적으로 지정
+  
+  if (response?.status === 401 || response?.status === 419) {
+    window.alert(`${data.msg}\n로그인 해주세요`)
+    window.location.replace('/');
+  }
+  return Promise.reject(error);
+
+}
+
+api.interceptors.response.use(onResponse, onResponseError)
 export default api;
