6df5644eb63ba9ae9441b8b74f456379b7cb35f3
yujung <ujunglim@naver.com>
2024-02-25 22:14:45 +0900

    [fix] api axios

--- Client/src/api/index.ts
+++ Client/src/api/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosResponse } from 'axios'; 
+import axios, { AxiosError, AxiosHeaders, AxiosResponse, InternalAxiosRequestConfig } from 'axios'; 
 import getCookies from '../utils/getCookies';
 
 const getBaseUrl = () => {
@@ -17,36 +17,48 @@ const getBaseUrl = () => {
 
 const api: any = axios.create(
   {
-    baseURL: getBaseUrl()
-    // baseURL: 'http://localhost:9090' // 로컬에서 dev할때
+    // baseURL: getBaseUrl()
+    baseURL: 'http://localhost:9090' // 로컬에서 dev할때
   }
 )
-// AxiosRequestConfig
-api.interceptors.request.use((config: any) => {
-  const cookie = getCookies();
-  config.headers.Authorization = cookie.jwt;
-  return config;
-})
 
 // 응답완료 interceptor
 const onResponse = (
   response: AxiosResponse,
 ): AxiosResponse | Promise<AxiosResponse> => {
   // access token 갱신 시 토큰 갱신
-  if (response.status === 201) {
-    localStorage.setItem('accessToken', response.data.accessToken);
-  }
+  // if (response.status === 201) {
+  //   localStorage.setItem('accessToken', response.data.accessToken);
+  // }
   return response;
 };
+
+const refreshToken = async () => {
+  try {
+    const res = await axios.get('/user/refresh'); // accessToken 갱신요청
+    console.log(res, '====')
+  } catch(err) {
+    throw err
+  }
+}
 
 // 응답에러 interceptor
 const onResponseError = (err: AxiosError | Error): Promise<AxiosError> => {
   const error = err as unknown as AxiosError;
   const { response } = error;
+
   const data = response?.data as { msg: string }; // 타입을 명시적으로 지정
-  
-  if (response?.status === 401 || response?.status === 419) {
-    window.alert(`${data.msg}\n로그인 해주세요`)
+
+  // accessToken이 만료
+  if (response?.status === 419) {
+    // refresh
+    refreshToken();
+    
+  }
+
+  // access, refresh 둘 다 만료시 로그인
+  if (response?.status === 401) {
+    window.alert(`${data.msg}\n로그인 해주세요`);
     window.location.replace('/');
   }
   return Promise.reject(error);
