09acc53089207282197ee2ccde591ed3d8062bb4
yujung <ujunglim@naver.com>
2024-03-12 14:34:43 +0900

    [feat] redux-persist로 redux데이터 로컬스토리지에 저장

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,14 +2,17 @@ import { useRoutes } from "react-router";
 import AppLayout from "./layout";
 import routes from "./config/routes";
 import { Provider } from "react-redux";
-import store from "./store";
+import store, { persistor } from "./store";
 import "react-toastify/dist/ReactToastify.css";
 import "./variables.scss";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <AppLayout>{useRoutes(routes)}</AppLayout>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppLayout>{useRoutes(routes)}</AppLayout>
+      </PersistGate>
     </Provider>
   );
 }
--- Client/src/store/index.ts
+++ Client/src/store/index.ts
@@ -1,14 +1,27 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore } from '@reduxjs/toolkit';
 import app from './slices/app';
 import folder from './slices/folder';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root', 
+  storage, // 로컬 스토리지에 저장
+};
+
+const rootReducer = combineReducers({
+  app,
+  folder,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: {
-      app, folder
-    },
+    reducer: persistedReducer,
 });
 
 export default store;
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
